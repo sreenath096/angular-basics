1a4804f50c60590f406966ef7a1366b5b6e7bf36
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-03 16:00:43 +0530

    Difference between ngOnChanges vs ngDoCheck hook

--- src/app/life-cycle-hooks/life-cycle-hooks.component.ts
+++ src/app/life-cycle-hooks/life-cycle-hooks.component.ts
@@ -8,7 +8,8 @@ import { Component, DestroyRef, Input, SimpleChanges } from '@angular/core';
   styleUrl: './life-cycle-hooks.component.css'
 })
 export class LifeCycleHooksComponent {
-  @Input() title = '';
+  @Input() title = { name: '' };
+  @Input() counter = 0;
   name = '';
   constructor(private destroyRef: DestroyRef) {
     console.log('constructor called');
@@ -20,14 +21,21 @@ export class LifeCycleHooksComponent {
   }
 
   ngOnInit() {
-    console.log(this.title);
+    //console.log(this.title);
     console.log('ngoninit fired');
-    this.name = this.title + 'added on nginint';
+    setTimeout(() => {
+      console.log('set time out');
+      this.name = this.title.name + 'added on nginint';
+    }, 2000);
   }
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngonchange fired');
     console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log('ngdocheck fired');
   }
 
   ngOnDestroy() {
@@ -37,7 +45,10 @@ export class LifeCycleHooksComponent {
   // Constructor called first
   // Input properties will get initialized
   // ngOnchanges - Runs will any input properties value changes
+  // Works for simple type, For reference type, on change of reference will trigger
+  // property name changes in reference type will not trigger the ngOnChanges
   // ngOnInit will get called  
-  // Template will get initialized
-
+  // ngDoCheck - Will fired whenever template changeDetection triggers
+  // Will fired when Input property changes. Avoid using it 
+  // Template will get initialized  
 }
