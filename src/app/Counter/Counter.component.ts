d1606cd908abba6e78c16939310efefca8b9a265
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-03 14:16:54 +0530

    Input Properties Component Data Exchange

---
+++ src/app/Counter/Counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { changeValue, trimValue } from './Counter.service';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+})
+export default class CounterComponent {
+  @Input({ required: true, transform: changeValue }) count: number = 0;
+
+  @Input({ transform: trimValue }) message: string = '';
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import ProfileComponent from './profile.component';
+import CounterComponent from './Counter/Counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileComponent],
+  imports: [RouterOutlet, ProfileComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-basics';
+  counter: number = 20;
 }
