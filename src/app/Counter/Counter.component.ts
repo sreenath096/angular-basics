181a5c9d65c939211f835b7b7391b15f1d0590b1
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-03 14:34:06 +0530

    Input Property with Type Checking & Built-in Transforms

--- src/app/Counter/Counter.component.ts
+++ src/app/Counter/Counter.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 import { changeValue, trimValue } from './Counter.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   standalone: true,
+  imports: [CommonModule],
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css'],
@@ -12,4 +14,6 @@ export default class CounterComponent {
   @Input({ required: true, transform: changeValue }) count: number = 0;
 
   @Input({ transform: trimValue }) message: string = '';
+
+  @Input({ transform: booleanAttribute }) showcounter: boolean = false;
 }
