ea956f2802d412b64fb62b2f71a0ddaf00029803
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-03 14:49:25 +0530

    Getters and Setters for Input Properties in Inherited Components

--- src/app/Counter/Counter.component.ts
+++ src/app/Counter/Counter.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input, booleanAttribute } from '@angular/core';
 import { changeValue, trimValue } from './Counter.service';
 import { CommonModule } from '@angular/common';
+import { GetterInputComponent } from '../getter-input/getter-input.component';
 
 
 @Component({
-  standalone: true,
-  imports: [CommonModule],
-  selector: 'app-counter',
-  templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css'],
+    standalone: true,
+    imports: [CommonModule],
+    selector: 'app-counter',
+    templateUrl: './counter.component.html',
+    styleUrls: ['./counter.component.css'],
+    inputs: ['count', 'message', 'showcounter', 'title: counterTitle']
 })
-export default class CounterComponent {
-  @Input({ required: true, transform: changeValue }) count: number = 0;
+export default class CounterComponent extends GetterInputComponent {
+    @Input({ required: true, transform: changeValue, alias: 'dummynamecounter' }) count: number = 0;
 
-  @Input({ transform: trimValue }) message: string = '';
+    @Input({ transform: trimValue }) message: string = '';
 
-  @Input({ transform: booleanAttribute }) showcounter: boolean = false;
+    @Input({ transform: booleanAttribute }) showcounter: boolean = false;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import ProfileComponent from './profile.component';
 import CounterComponent from './Counter/Counter.component';
+import { GetterInputComponent } from './getter-input/getter-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileComponent, CounterComponent],
+  imports: [RouterOutlet, ProfileComponent, CounterComponent, GetterInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
